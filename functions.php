881b73d7715ca5ca87d70acb5511c686c50722d1
Lucapandolfelli <lucapandolfelli2002@gmail.com>
2021-06-15 20:41:06 -0300

    Se configuró Laravel para compilar CSS y JS

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@
 
     function register_styles(){
 
-        wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array('bootstrap'), '1.0,', 'all');
+        wp_enqueue_style('style', get_template_directory_uri() . '/public/css/app.css', array(), '1.0,', 'all');
         wp_enqueue_style('bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css", array(), '5.0.1', 'all');
         wp_enqueue_style('fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css", array(), '5.15.3', 'all');
 
@@ -24,6 +24,7 @@
 
     function register_scripts(){
         
+        wp_enqueue_script('javascript', get_template_directory_uri() . '/public/js/app.js', array(), '1.0', true);
         wp_enqueue_script('bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js", array(), '5.0.1', true);
         wp_enqueue_script('poppins', "https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js", array(), '2.9.2', true);
 
