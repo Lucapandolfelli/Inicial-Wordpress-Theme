c8f4c69f1269a5a4aecd5e051a0432d8cb5659fc
Lucapandolfelli <85750567+Lucapandolfelli@users.noreply.github.com>
2021-06-15 22:21:22 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -4,13 +4,13 @@
     <div class="container px-5 my-4">
         <?php get_template_part('/template-parts/content', 'title'); ?>
         <div class="row">
-                <div class="col-xs-12 col-sm-9 d-flex justify-content-center align-items-center"> 
-                    <h1 class="text-center text-uppercase">Page not found!</h1>
-                </div>
-                <div class="col-xs-12 col-sm-3">
-                    <?php get_sidebar(); ?>
-                </div>
+            <div class="col-xs-12 col-sm-9 d-flex justify-content-center align-items-center"> 
+                <h1 class="text-center text-uppercase">Page not found!</h1>
             </div>
+            <div class="col-xs-12 col-sm-3">
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
     </div>
 </main>
 
